4cb61d54c1c54ea0d121aa996d46b2de6be67338
davisjeffrey <jeff.davis@att.net>
2024-02-19 15:17:56 -0600

    useData Hook

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,11 +1,11 @@
-import React from "react";
+import useData from "../Hooks/useData";
 import useGenres from "../Hooks/useGenres";
 
 const GenreList = () => {
-  const { genres } = useGenres();
+  const { data } = useGenres();
   return (
     <ul>
-      {genres.map((genre) => (
+      {data.map((genre) => (
         <li key={genre.id}>{genre.name}</li>
       ))}
     </ul>
